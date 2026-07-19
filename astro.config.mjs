05db84ee593033cf3b272d5acfdbe01e5a7c07f6
Jiří Novák <novis1928@gmail.com>
2026-07-19 11:52:30 +0200

    Add SEO sitemap and robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://novis1928.github.io",
   base: "/weight-loss-without-starving",
+
+  integrations: [sitemap()],
 });
